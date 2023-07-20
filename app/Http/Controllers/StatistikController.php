8527ab20088503ca76a41d5d6f0e69ba7c14f267
Agung_Pradana_08 <izunna158@gmail.com>
2023-07-20 07:48:32 +0700

    [Agung] Adding Statistik

---
+++ app/Http/Controllers/StatistikController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StatistikController extends Controller
+{
+    // public function index(){
+    //     $statistik = Satistik::all();
+    // }
+}
